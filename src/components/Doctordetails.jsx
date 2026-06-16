51cc3c04c842e9584b827ced5a6914f7f1459d14
triveni-33 <boyapatitriveni5@gmail.com>
2026-06-16 16:02:36 +0530

    first commit

--- src/components/Doctordetails.jsx
+++ src/components/Doctordetails.jsx
@@ -1,10 +1,56 @@
-import   { useEffect , useState} from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 function Doctordetails() {
-    const {id} = useParams()
+  const {id}=useParams()
+  let [doctor,setDoctor]=useState({})
+  useEffect(()=>{
+    fetchdata()
+  },[])
+
+  function fetchdata(){
+    let data=[
+      {
+        id: 1,
+        name: "Teja",
+        age: 26,
+        gender: "Male",
+        specialization: "Muscles",
+        salary: 7000000,
+      },
+
+      {
+        id: 2,
+        name: "Sam",
+        age: 26,
+        gender: "Male",
+        specialization: "Bones",
+        salary: 4000000,
+      },
+
+      {
+        id: 3,
+        name: "Anu",
+        age: 25,
+        gender: "Female",
+        specialization: "Heart",
+        salary: 5000000,
+      },
+    ];
+
+    let finddoctor=data.find(val=>{
+        return val.id==id
+    })
+    setDoctor(finddoctor)
+  }
   return (
-    <div>Doctordetails {id} </div>
+    <div>
+        <h1>{doctor.id}</h1>
+        <h1>{doctor.name}</h1>
+        <h1>{doctor.salary}</h1>
+        <h1>{doctor.gender}</h1>
+        <h1>{doctor.specialization}</h1>
+    </div>
   )
 }
 
